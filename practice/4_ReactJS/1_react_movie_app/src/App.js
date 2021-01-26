356e25aea238752c7e3485fa5df113067172bfb7
Seok93 <gkdltjr@example.com>
2021-01-26 10:35:59 +0900

    add Navigation & Component props

--- practice/4_ReactJS/1_react_movie_app/src/App.js
+++ practice/4_ReactJS/1_react_movie_app/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { HashRouter, Route } from "react-router-dom";
-import About from "./routes/About";
 import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
 import Navigation from "./components/Navigation";
 import "./App.css";
 
@@ -10,7 +11,8 @@ function App() {
     <HashRouter>
       <Navigation />
       <Route path="/" exact={true} component={Home} />
-      <Route path="/about" component={About} />
+      <Route path="/about" exact={true} component={About} />
+      <Route path="/movie/:id" exact={true} component={Detail} />
     </HashRouter>
   );
 }
